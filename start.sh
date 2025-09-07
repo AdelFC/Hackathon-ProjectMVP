5dce8123ec3e93555c8c85fdede52cf510513c94
Enzo NOLLET <ennollet@f1br8s19.clusters.42paris.fr>
2025-09-07 16:30:49 +0200

    fix

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 # create a start shell that run the docker compose up --build and check if docker is installed
 #!/bin/bash
-if ! [ -x "$(command -v docker)" ]; then
-  echo 'Error: docker is not installed.' >&2
-  exit 1
-fi
-if ! [ -x "$(command -v docker compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
 
 docker compose up --build
